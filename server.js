54208fdbe861b37d6064aca3ecbb1d0535f10412
J@tin <ezedin.fedlu@gmail.com>
2021-12-24 22:59:14 +0300

    add auth to json-server

---
+++ server.js
@@ -0,0 +1,23 @@
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+
+const app = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
+// /!\ Bind the router db to the app
+app.db = router.db
+
+// You must apply the auth middleware before the router
+app.use(auth)
+app.use(router)
+app.listen(3000, () => console.log("port is listening at port 3000..."));
