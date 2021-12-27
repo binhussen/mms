5565942f86de7be32f7de6ae9278f5e3cb463617
J@tin <ezedin.fedlu@gmail.com>
2021-12-27 19:23:51 +0300

    quick fix

--- src/app/mms-common/organisms/table/table.component.ts
+++ src/app/mms-common/organisms/table/table.component.ts
@@ -1,19 +1,25 @@
-import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
-import {FormDialogComponent} from "../form-dialog/form-dialog.component";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatTableDataSource} from "@angular/material/table";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {Form} from "../../models/form";
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { FormDialogComponent } from '../form-dialog/form-dialog.component';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Form } from '../../models/form';
 export interface Action {
   name: string;
-  type: 'expand' | 'edit' | 'delete',
+  type: 'expand' | 'edit' | 'delete';
   path?: string; // if expand we redirect the user to detail page. For example: if path = 'post' and item id = 2 => /post/2
 }
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit, AfterViewInit {
   data!: Array<any>; // or url
@@ -37,46 +43,51 @@ export class TableComponent implements OnInit, AfterViewInit {
 
   async ngOnInit() {
     this.loading = true;
-   await this.getData('0', this.pageSize.toString());
+    await this.getData('0', this.pageSize.toString());
   }
   openDialog() {
-    const dialogRef = this.dialog.open(
-      FormDialogComponent,
-      {
-        width: '75%',
-        maxWidth: '100vw',
-        disableClose: true,
-        data: {actionTitle: "Create", form: this.form }
-      }
-    );
-
+    const dialogRef = this.dialog.open(FormDialogComponent, {
+      width: '75%',
+      maxWidth: '100vw',
+      disableClose: true,
+      data: { actionTitle: 'Create', form: this.form },
+    });
   }
-  command(actionType: 'expand' | 'edit' | 'delete', row: any){
+  command(actionType: 'expand' | 'edit' | 'delete', row: any) {
     // TODO: pass the command to parent template or page
     console.log(actionType, row);
   }
 
-  getColumns(data: Array<any>, excluded: Array<string> = ['created_at', 'updated_at'], actionsAvailable: boolean) {
+  getColumns(
+    data: Array<any>,
+    excluded: Array<string> = ['created_at', 'updated_at'],
+    actionsAvailable: boolean
+  ) {
     let columns = data
       .reduce((c, r) => {
-        return [...c, ...Object.keys(r)]
+        return [...c, ...Object.keys(r)];
       }, [])
       .reduce((cs: any, c: any) => {
-        return cs.includes(c)
-          ? cs
-          : [...cs, c]
+        return cs.includes(c) ? cs : [...cs, c];
       }, [])
-      .filter((c: any) => !excluded.includes(c))
+      .filter((c: any) => !excluded.includes(c));
     columns = ['No', ...columns, ' '];
     // Describe the columns for <mat-table>.
     this.columns = columns.map((column: any, index: number) => {
       return {
         columnDef: column,
         header: column.replace(/([^A-Z])([A-Z])/g, '$1 $2'),
-        cell: (element: any) => `${column === 'No' ? '' : (element && element[column] ? element[column] : ``)}`,
-      }
-    })
-    this.displayedColumns = this.columns.map(c => c.columnDef);
+        cell: (element: any) =>
+          `${
+            column === 'No'
+              ? ''
+              : element && element[column]
+              ? element[column]
+              : ``
+          }`,
+      };
+    });
+    this.displayedColumns = this.columns.map((c) => c.columnDef);
   }
 
   async getData(page: string, limit: string, currentSize?: number) {
@@ -84,30 +95,33 @@ export class TableComponent implements OnInit, AfterViewInit {
     params = params.set('_page', page);
     params = params.set('_limit', limit);
     this.httpClient
-      .get<Array<any>>(`${this.dataSourceUrl}?${params.toString()}`)
-      .toPromise().then((data) => {
-      if (!currentSize) {
-        this.data = data;
-        this.getColumns(this.data, this.excludedColumns, true);
-      }
-      if (currentSize) {
-        this.data.length = currentSize;
-        this.data.push(...data);
-      }
-      this.data.length = 20;
-      this.dataSource = new MatTableDataSource<any>(this.data);
-      this.dataSource._updateChangeSubscription();
-      this.dataSource.paginator = this.paginator;
-      this.loading = false;
-    });
+      .get<Array<any>>(`${this.dataSourceUrl}?${params.toString()}`, {
+        observe: 'response',
+      })
+      .toPromise()
+      .then((data) => {
+        if (!currentSize) {
+          this.data = data.body ?? [];
+          this.getColumns(this.data, this.excludedColumns, true);
+        }
+        if (currentSize) {
+          const temp = data.body ?? [];
+          this.data.length = currentSize;
+          this.data.push(...temp);
+        }
+        this.data.length = Number(data.headers.get('X-Total-Count') ?? 20);
+        this.dataSource = new MatTableDataSource<any>(this.data);
+        this.dataSource._updateChangeSubscription();
+        this.dataSource.paginator = this.paginator;
+        this.loading = false;
+      });
   }
 
   async pageChange(value: any) {
     const _page = value.pageIndex;
     const _limit = value.pageSize;
-    let previousSize = (_page) * _limit;
+    let previousSize = _page * _limit;
     await this.getData(_page + 1, _limit, previousSize);
-
   }
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
